7a2b7377ecdf184fabed49e7a9f93afa27f07b8c
stovet <steven.topalian@gmail.com>
2022-01-11 20:34:57 -0500

    some css

--- src/components/AdDesigner.jsx
+++ src/components/AdDesigner.jsx
@@ -10,29 +10,24 @@ function AdDesigner(){
     let disabled2 = "";
     if(theme){
         darkTheme = "darkTheme";
-        disabled1 = "disabled";
-        disabled2 = "";
-    } else {
-        disabled1 = "";
-        disabled2 = "disabled";
     }
     return (
         <div className="AdDesigner">
             <h1>Ad Designer</h1>
             <div className={"voteFor " + darkTheme}>
-                <p>Vote For {flavor}</p>
+                <p>Vote For</p> <span>{flavor}</span>
             </div>
-            <h2>What to Support</h2>
+            <h3>What to Support</h3>
             <button onClick={() => setFlavor("Chocolate")}>Chocolate</button>
             <button onClick={() => setFlavor("Vanilla")}>Vanilla</button>
             <button onClick={() => setFlavor("Strawberry")}>Strawberry</button>
-            <h2>Color Theme</h2>
+            <h3>Color Theme</h3>
             
             
             <button onClick={() => setTheme(false)} disabled1>Light</button> 
             <button onClick={() => setTheme(true)} disabled2>Dark</button>
             
-            <h2>Font Size</h2>
+            <h3>Font Size</h3>
             <button onClick={() => setFontSize(fontSize - 1)}>Down</button>
             <span>{fontSize}</span>
             <button onClick={() => setFontSize(fontSize + 1)}>Up</button>

d763eb1782db5e072d4f805090923f6712e11f90
stovet <steven.topalian@gmail.com>
2022-01-12 13:52:14 -0500

    spacing fixed for 3 at top

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,11 @@ function App() {
   return (
     <div className="App">
       <Header user="Steve"/>
+      <div className="container">
       <Ad flavor="Chocolate" fontSize={20} darkTheme={true}/>
       <Ad flavor="Vanilla" fontSize={12} darkTheme={false}/>
       <Ad flavor="Strawberry" fontSize={25} darkTheme={false}/>
+      </div>
       <AdDesigner />
       <Votes />
     </div>
